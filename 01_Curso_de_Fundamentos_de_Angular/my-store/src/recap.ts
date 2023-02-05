239e15116833938675f2f43036d9ce029db005a8
yfsanchez <yfsanchez@gmail.com>
2023-02-04 21:44:52 -0300

    primeros ejemplos
    primeros ejemplos

---
+++ 01_Curso_de_Fundamentos_de_Angular/my-store/src/recap.ts
@@ -0,0 +1,13 @@
+const username : string | number = 'yfsanchez';
+const sum = (a: number, b: number) => {
+  return a + b;
+}
+
+class Person {
+  constructor(public age: number,  public lastName:string) {
+
+  }
+}
+
+const yfs = new Person(23,'dfghdf');
+yfs.age;
