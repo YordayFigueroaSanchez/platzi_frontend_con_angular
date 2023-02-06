df0c6c16ab271499b415f5fd29513a04b9445c21
yfsanchez <yfsanchez@gmail.com>
2023-02-06 09:22:19 -0300

    ngfor productos
    ngfor productos

--- 01_Curso_de_Fundamentos_de_Angular/my-store/src/app/app.component.ts
+++ 01_Curso_de_Fundamentos_de_Angular/my-store/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Product } from './product.model';
 
 @Component({
   selector: 'app-root',
@@ -18,6 +19,40 @@ export class AppComponent {
   }
   names : string[] = ['name01','name02','name03','name04',]
   newName : string = '';
+
+  products : Product[] = [
+    {
+      name: 'EL mejor juguete',
+      price: 565,
+      image: './assets/images/01.jpg',
+      category: 'all',
+    },
+    {
+      name: 'Bicicleta casi nueva',
+      price: 356,
+      image: './assets/images/02.jpg'
+    },
+    {
+      name: 'Colleción de albumnes',
+      price: 34,
+      image: './assets/images/03.jpg'
+    },
+    {
+      name: 'Mis libros',
+      price: 23,
+      image: './assets/images/04.jpg'
+    },
+    {
+      name: 'Casa para perro',
+      price: 34,
+      image: './assets/images/05.jpg'
+    },
+    {
+      name: 'Gafas',
+      price: 3434,
+      image: './assets/images/06.jpg'
+    }
+  ]
 
   onToggle(){
     this.btnDisabled = !this.btnDisabled;
